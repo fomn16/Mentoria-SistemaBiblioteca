7e102b773d6cd42b7432683fd46020d98e208535
felipe.neves <felipe.neves@websis.com.br>
2024-11-26 17:39:17 -0300

    teste2

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+//mudei outra coisa que n vai pro git
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
